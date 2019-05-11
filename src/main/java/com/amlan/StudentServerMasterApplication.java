33b906d80c235998094e43cc3d38ab72a6bd7490
amlan2 <50303264+amlan2@users.noreply.github.com>
2019-05-11 14:07:49 +0530

    student server initial

---
+++ src/main/java/com/amlan/StudentServerMasterApplication.java
@@ -0,0 +1,13 @@
+package com.amlan;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudentServerMasterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StudentServerMasterApplication.class, args);
+	}
+
+}
